81ae605eb158b4d0feadf29b08ce9e73edafcffd
Ignacio Vissani <ivissani@gmail.com>
2012-03-22 11:05:37 -0300

    added minisat-2.2.0 modified

---
+++ minisat/minisat-2.2.0/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+export MROOT=`pwd`
+cd core
+make clean
+make rs
---
+++ minisat/minisat-2.2.0/core/depend.mk
@@ -0,0 +1,29 @@
+/home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Main.o /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Main.or /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Main.od /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Main.op: /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Main.cc \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/utils/System.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/IntTypes.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/utils/ParseUtils.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/utils/Options.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Vec.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/XAlloc.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Dimacs.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/SolverTypes.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Alg.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Map.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Alloc.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Solver.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Heap.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Sort.h
+/home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Solver.o /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Solver.or /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Solver.od /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Solver.op: \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Solver.cc \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Sort.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Vec.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/IntTypes.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/XAlloc.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/Solver.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Heap.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Alg.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/utils/Options.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/utils/ParseUtils.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/core/SolverTypes.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Map.h \
+ /home/ivissani/RFM/miscosas/minisat/minisat-2.2.0/mtl/Alloc.h
