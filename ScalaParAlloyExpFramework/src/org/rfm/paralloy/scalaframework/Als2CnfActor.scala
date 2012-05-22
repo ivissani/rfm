9575744b969a2105652346d6f71ba400c6de9b10
Ignacio Vissani <ivissani@gmail.com>
2012-05-22 13:40:49 -0300

    nothing important

--- ScalaParAlloyExpFramework/src/org/rfm/paralloy/scalaframework/Als2CnfActor.scala
+++ ScalaParAlloyExpFramework/src/org/rfm/paralloy/scalaframework/Als2CnfActor.scala
@@ -10,51 +10,62 @@ import edu.mit.csail.sdg.alloy4compiler.parser.CompUtil
 import edu.mit.csail.sdg.alloy4compiler.translator.TranslateAlloyToKodkod
 import edu.mit.csail.sdg.alloy4compiler.translator.A4Solution
 import scala.util.parsing.combinator.JavaTokenParsers
+import edu.mit.csail.sdg.alloy4.A4Reporter
 
 class Als2CnfActor extends Actor {
 	def act() {
+	  // Defines a new actor who will translate the ALS while
+	  // the main actor continues to listen for new requests.
+	  // This way this is scalable by adding more working threads.
 	  def translateAsynchronous(alsfile : File, actor : Actor) {
 		  val mainActor = Actor.self
 		  
 		  Actor.actor {
-		    Thread.sleep(1000)
 		    mainActor ! ("TRANSLATION FINISHED", alsfile, actor, translate(alsfile))
 		  }
 	  }
 	  
 	  loop {
 	    react {
-	      case (alsfile : File, actor : Actor) => translateAsynchronous(alsfile, actor) 
+	      // new translation request incoming
+	      case (alsfile : File, actor : Actor) => translateAsynchronous(alsfile, actor)
+	      
+	      // one translation is ready
 	      case ("TRANSLATION FINISHED", alsfile : File, actor : Actor, translation : String) => actor ! translation
 	    }
 	  }
 	}
 	
 	def translate(f : File) : String = {
+	   
 	  object headerparser extends JavaTokenParsers {
 		  def amount = decimalNumber ^^ (_.toInt)
 		  def header = "p cnf "~amount~amount ^^ (a => (a._1._2, a._2))
 	  }
 	  
+	  // Redirect standard output so we can catch the translation
+	  // Super warning, this won't work if multiple fellows are writing to stdout concurrently
 	  var baos = new ByteArrayOutputStream()
 	  var pstream = new PrintStream(baos, true)
 	  var oldout = new PrintStream(System.out)
 	  
 	  System.setOut(pstream)
 	  
-	  a2cmain(List(f.getAbsolutePath).toArray)
+	  // Translate!
+	  val translation = a2cmain(List(f.getAbsolutePath).toArray)
 	  
+	  // Restore standard output
 	  System.setOut(oldout)
 	  Console.setOut(oldout)
 	  
-	  val lines = baos.toString().split("\n")
+	  val lines = translation.split("\n") // baos.toString().split("\n")
 	  val header = headerparser.parse(headerparser.header, lines(lines.length-1))
 	  val quasires = ("p cnf %d %d".format(header.get._1 - 1, header.get._2) ::lines.take(lines.length - 1).toList)
 	  
 	  quasires.mkString("\n")
 	}
 	
-	def a2cmain(paramArrayOfString : Array[String]) {
+	def a2cmain(paramArrayOfString : Array[String]) : String = {	  
 	   var localCommandLine = A2C.parseCommandLine(paramArrayOfString);
 	   var localA4Options = A2C.xlateCommandLine(localCommandLine);
 
@@ -69,8 +80,12 @@ class Als2CnfActor extends Actor {
 
 		   Console.err.println("Translating ...");
 		   answer = TranslateAlloyToKodkod.execute_command(null, localCompModule.getAllReachableSigs(), localCommand, localA4Options);
+		   return null
 	   } catch {
-	     case localException => localException.printStackTrace();
+	     case localException => {
+	       localException.printStackTrace();
+	       return null
+	     }
 	   }
 	}
 }
