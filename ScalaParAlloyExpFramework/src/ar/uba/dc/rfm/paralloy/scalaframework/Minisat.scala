bb1d7e528a647166973ca3a4c1001299803e3467
Ignacio Vissani <ivissani@gmail.com>
2012-08-11 20:33:05 -0300

    added comments

--- ScalaParAlloyExpFramework/src/ar/uba/dc/rfm/paralloy/scalaframework/Minisat.scala
+++ ScalaParAlloyExpFramework/src/ar/uba/dc/rfm/paralloy/scalaframework/Minisat.scala
@@ -10,22 +10,28 @@ class Minisat extends Solver {
 	var lastExEndTimeMillis : Long = 0
 	
     def solve_restricted(what : List[Clause], forTime : Double, forConflicts : Int, forPropagations : Int, withLearnts : List[LearntClause], assuming : List[Int]) : Char = {
-      set_budget_off()
+      set_budget_off() // This resets the budgets
       
+      // Add problem and learned clauses
+      // WARNING: Watch performance here, may be a memory black hole on big problems
 	  what.foreach(a => this.add_clause(a.toIntseq))
 	  withLearnts.foreach(a => a match {case LearntClause(act, cla, lbd) => this.add_learnt(cla.toIntseq, act.toFloat)})
 	  
+	  // Set solving limits. The solver stops if any of these limits is going to be surpassed
 	  set_conf_budget(forConflicts)
 	  set_prop_budget(forPropagations)
 	  set_time_budget(forTime)
 	  
+	  // Translate assumptions
 	  var is = new intseq
 	  assuming.foreach(is.add)
 	  
+	  // Execute measuring total time
 	  lastExStartTimeMillis = System.currentTimeMillis()
 	  def ret = solve_limited(is)
 	  lastExEndTimeMillis = System.currentTimeMillis()
 	  
+	  // @TODO : translate this into an enumerated value
 	  ret
 	}
     
